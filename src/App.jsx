9e03c7ef3abff7e442006c66b938ed324fe89868
tetyana1201 <thretscyn@gmail.com>
2026-04-17 12:51:24 +0300

    create survey page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Survey from './pages/Survey';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        
-        {
-          
-        }
+        <Route path="/survey" element={<Survey />} />
       </Routes>
     </Router>
   );
