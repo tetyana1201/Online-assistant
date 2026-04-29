5cdbe301556a642a3409f6a9db9be691085df4f8
tetyana1201 <thretscyn@gmail.com>
2026-04-29 23:29:44 +0300

    update main landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Survey from './pages/Survey';
-import About from './pages/About';
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import History from "./pages/History";
+import Assistant from "./pages/Assistant";
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/aiassistant" element={<Assistant />} />
       </Routes>
     </Router>
   );
